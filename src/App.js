d5cfc465246dd9677d0d34f835f8d0bd5d5a8b02
bds0900 <Dbeak9336@conestogac.on.ca>
2020-03-22 21:33:08 -0400

    use apolloprovider wrapthe app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 import './App.css';
+
+
+const client = new ApolloClient({
+  uri: 'https://pi-project-097921097e.herokuapp.com/pi-project/dev/',
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="App">
 
     </div>
+    </ApolloProvider>
   );
 }
 
