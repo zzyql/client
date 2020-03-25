7d6eee26259cbde49c9f484f91b77562373639d9
bds0900 <Dbeak9336@conestogac.on.ca>
2020-03-25 12:22:33 -0400

    update student list component

--- src/component/students/StudentList.tsx
+++ src/component/students/StudentList.tsx
@@ -1,13 +1,50 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, Fragment } from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag';
+import { NavLink } from 'react-router-dom';
+import { StudentType } from '../Interfaces';
 
-interface Props {
-    
+const GET_STUDENTS = gql`
+  query GET_STUDENTS($course_id:string){
+    students(course_id:$course_id){
+      id
+      first_name
+      last_name
+    }
+  }
+`;
+interface StudentListData {
+    students: StudentType[];
+}
+  
+interface StudentListVars {
+    course_id:string;
 }
 
-export default function StudentList({}: Props): ReactElement {
+interface Props {
+    match:any
+}
+
+export default function StudentList(props: Props): ReactElement {
+    const{loading,data}= useQuery<StudentListData,StudentListVars>(
+        GET_STUDENTS,
+        {variables:{course_id:props.match.params.id}}
+    );
     return (
         <div>
             Student list
+            {loading ? (
+                <p>Loading ...</p>
+              ) : (
+                <Fragment>
+                {"course list.."}
+                {data && data.students.map(student => (
+                    <li key={student.id} className="student-list"> 
+                        
+                    </li>
+                ))}
+                </Fragment>
+              )}
         </div>
     )
 }
