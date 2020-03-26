2654a8f87a9a181aa2c2a72435c029ae7b92f345
bds0900 <Dbeak9336@conestogac.on.ca>
2020-03-26 13:56:59 -0400

    add get course query

--- src/component/courses/Course.tsx
+++ src/component/courses/Course.tsx
@@ -1,11 +1,40 @@
 import React, { ReactElement } from 'react'
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { CourseType } from '../Interfaces';
 
+
+const Get_COURSE = gql`
+  query Get_COURSE($coure_id: string!) {
+    course(where: {id:$coure_id}) {
+      id
+      model
+      year
+      stock
+    }
+  }
+`;
+
+interface CourseData{
+    course:CourseType
+}
+interface CourseVars{
+    coure_id:string
+}
 interface Props {
     match:any
 }
 
+
 export default function Course(props: Props): ReactElement {
     console.log(props.match.params.id)
+
+    const { loading, data } = useQuery<CourseData, CourseVars>(
+        Get_COURSE,
+        { variables: { coure_id: props.match.params.id } }
+      );
+
+
     return (
         <div>
             a course
